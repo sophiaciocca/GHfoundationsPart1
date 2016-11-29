fa6c25041cf01e58a0cf569e683a4a8e91952188
Karen MacPherson <karen@fullstackacademy.com>
2016-11-29 07:21:44 -0500

    added comments and clear to factory spec

--- 02-Factory-Functions/calculatorSpec.js
+++ 02-Factory-Functions/calculatorSpec.js
@@ -1,21 +1,31 @@
-// In this exercise create a calculator object
+// In this exercise create a function that can
+// create and return a calculator object
 // that performs basic mathematic operations.
-// 
+ 
 // We'll be using a pattern called the factory pattern where
-// our function creates an object tailored to our needs.  This function
-// is just like the one we built in *01_properties*, except now we're
-// creating some useful functionality
+// our function, `createCalculator` creates an object tailored
+// to our needs.  
 
 describe("Create a Calculator", function() {
   var calculator;
 
-  // Here we're calling the `createCalculator` function, it returns
-  // a calculator object.
+  // Below we're calling the `createCalculator` function, it returns
+  // a value we are naming calculator. The tests below give us hints about
+  // what functionality 'calculator' should have.
+  
+  // This will happen before each `it` block of tests,
+  // which means each set of tests will start with a new calculator variable.
   beforeEach(function() {
     calculator = createCalculator();
   });
 
-  it("initially has 0", function() {
+  it("initially has a value of 0", function() {
+    
+    //This line tells us a lot!
+    //First, it tells us that `calculator` must be an object that has a method called value
+    //Second, this tells us that its property `value` is a function, and should return the 
+    //outcome of the calculations
+    //hmm.. I guess we'll need to store those outcomes somewhere..
     expect(calculator.value()).toEqual(0);
   });
 
@@ -47,4 +57,12 @@ describe("Create a Calculator", function() {
     calculator.subtract(2);
     expect(calculator.value()).toEqual(1);
   });
+
+  it("can clear its value", function() {
+    calculator.add(5);
+    calculator.add(10);
+    calculator.subtract(7);
+    calculator.clear();
+    expect(calculator.value()).toEqual(0);
+  })
 });
