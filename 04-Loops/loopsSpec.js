f6f8d0ce8c1ccadcbb5e3fe4830e1190b3be583c
Karen MacPherson <karen@fullstackacademy.com>
2016-11-29 08:34:03 -0500

    update rename files

--- 04-Loops/loopsSpec.js
+++ 04-Loops/loopsSpec.js
@@ -248,13 +248,20 @@ describe("looping over objects", function() {
     })
   });
 
-  xdescribe('renameFiles', function(){
+  xdescribe('The function `renameFiles`', function(){
 
     /*
 
     You are given an array of desired filenames in the order of their creation. Since two files cannot have equal names, the one which comes later will have an addition to its name in a form of (n), where "n" is the smallest positive integer such that the obtained name is not used yet.
 
     Return an array of names that will be given to the files.
+
+    Example:
+
+    var files = ['myFile', 'anotherFile', 'family-picture', 'myFile', 'another-file', 'myFile'];
+    var newFileNames = renameFiles(files);
+    console.log(newFilesNames);
+    --> ['myFile', 'anotherFile', 'family-picture', 'myFile(1)', 'anotherFile(1)', 'myFile(2)']
     
     */
 
@@ -267,10 +274,17 @@ describe("looping over objects", function() {
       expect(renameFiles(['FullstackTestFirst', 'GuessingGame', 'FileWatcher'])).toEqual(['FullstackTestFirst', 'GuessingGame', 'FileWatcher']);
     });
 
-    it('Renames files if there are duplicates by adding `(num)` to the end of the filename where `num` is the smallest positive integer that the obtained name did not use.', function(){
+    it('renames files if there are duplicates by adding `(n)` to the end of the filename where `n` is the smallest positive integer that the obtained name did not use.', function(){
       expect(renameFiles(['hello', 'world', 'hello'])).toEqual(['hello', 'world', 'hello(1)']);
-      expect(renameFiles(["a(1)","a(6)","a","a","a","a","a","a","a","a","a","a"])).toEqual(["a(1)","a(6)","a","a(2)","a(3)","a(4)","a(5)","a(7)","a(8)","a(9)","a(10)","a(11)"]);
     });
+    
+    it('does not rename files to names that are already taken', function() {
+      expect(renameFiles(["a(1)","a(6)","a","a","a","a","a","a","a","a","a","a"])).toEqual(["a(1)","a(6)","a","a(2)","a(3)","a(4)","a(5)","a(7)","a(8)","a(9)","a(10)","a(11)"]);
+      //This definitely increases the difficulty of the problem! If a file already exists, you can't use that name. For example, if file(3) already exists, you shouldn't name another file 'file(3)'.
+    })
+    
+
+    //This is a tricky one! You will need to use all the tools at your disposal.  
 
   });
 });
